35a7d78c8f9df51cfda71c90c3921c061e58db56
herrluk <herrluk@example.com>
2022-07-27 19:02:55 +0800

    新增关于界面

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func handleFunc(w http.ResponseWriter,r *http.Request){
-	fmt.Fprint(w,"<h1>hello,这里是 goblog</h1>")
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>hello,这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或者建议，请联系 "+
+			"<a href=\"mailto:herrluk@example.com\">herrluk@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
+	}
 }
-func main(){
-	http.HandleFunc("/",handleFunc)
-	http.ListenAndServe(":3000",nil)
+func main() {
+	http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3000", nil)
 }
